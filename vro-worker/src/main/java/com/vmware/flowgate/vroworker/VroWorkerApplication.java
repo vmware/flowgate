b50e070c152337dfaa8d35643e7cc23de393f195
yixingj <yixingj@vmware.com>
2019-04-01 16:06:31 +0800

    Fix scan package error

--- vro-worker/src/main/java/com/vmware/flowgate/vroworker/VroWorkerApplication.java
+++ vro-worker/src/main/java/com/vmware/flowgate/vroworker/VroWorkerApplication.java
@@ -7,7 +7,7 @@ package com.vmware.flowgate.vroworker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages={"com.vmware.wormhole"})
+@SpringBootApplication(scanBasePackages={"com.vmware.flowgate"})
 public class VroWorkerApplication {
 
 	public static void main(String[] args) {
