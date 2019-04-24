ef29570d09c41f84733ee2c05f99a01d85f9f980
yixingj <yixingj@vmware.com>
2019-04-24 13:22:08 +0800

    Update the default key

--- flowgate-api/src/test/java/com/vmware/flowgate/util/FlowgateKeystoreTest.java
+++ flowgate-api/src/test/java/com/vmware/flowgate/util/FlowgateKeystoreTest.java
@@ -3,12 +3,12 @@
  */
 package com.vmware.flowgate.util;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import com.vividsolutions.jts.util.Assert;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
@@ -16,6 +16,6 @@ public class FlowgateKeystoreTest {
 
    @Test
    public void testGuardStore() {
-      Assert.isTrue(FlowgateKeystore.getEncryptKey().length() == 16);
+      Assert.assertTrue(FlowgateKeystore.getEncryptKey().length() == 16);
    }
 }
