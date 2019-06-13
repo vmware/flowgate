99cd41e19119c0bdbb746de2c6f82ab0888ac7a8
haoyul <haoyul@vmware.com>
2019-06-13 14:30:06 +0800

    fix build script

--- make/database/init.sh
+++ make/database/init.sh
@@ -7,6 +7,18 @@
 /opt/couchbase/bin/couchbase-cli bucket-create -c localhost -u flowgate_cluster -p ADMINPASSWD_CHANGE --bucket flowgate --bucket-type couchbase --bucket-ramsize 4096 --bucket-replica 1 --wait
 /opt/couchbase/bin/couchbase-cli user-manage -c localhost -u flowgate_cluster -p ADMINPASSWD_CHANGE --set --rbac-username=flowgate --rbac-password=USERPASSWD_CHANGE --roles=bucket_full_access[*] --auth-domain=local
 
+# Wait for the query service to be up and running
+for attempt in $(seq 5)
+do
+    RET=`cbq -u flowgate_cluster -p ADMINPASSWD_CHANGE --script="select count(*) from flowgate;" | grep -A 3 results | grep '"$1":'`
+    if [ -n "$RET" ] && [ ${RET#*:} = "0" ];then
+        echo "Service start success."
+        break
+    fi
+    echo "Waiting for query service..."
+    sleep 1
+done
+
 ###create index###
 cbq -u flowgate_cluster -p ADMINPASSWD_CHANGE -file /home/couchbase/initData.sh > /opt/couchbase/var/couchbase_initialize.log
 
