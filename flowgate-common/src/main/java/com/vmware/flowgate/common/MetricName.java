6c97ee4d5f43c387c9f6447bda3d67fbc3d04ce0
Pengpeng Wang <46948228+Pengpengwanga@users.noreply.github.com>
2020-02-21 17:35:54 +0800

    Refactor the model of ValueUnit (#264)
    Signed-off-by: Pengpeng Wang <pengpengw@vmware.com>

---
+++ flowgate-common/src/main/java/com/vmware/flowgate/common/MetricName.java
@@ -0,0 +1,30 @@
+package com.vmware.flowgate.common;
+
+public class MetricName {
+   public static final String TEMPERATURE = "Temperature";
+   public static final String HUMIDITY = "Humidity";
+   public static final String AIRPRESSURE = "Airpressure";
+   public static final String CONTACTCLOSURE = "Contactclousre";
+   public static final String SMOKE = "Smoke";
+   public static final String VIBRATION = "Vibraion";
+   public static final String PDU_VOLTAGE = "Voltage";
+   public static final String PDU_ACTIVE_POWER = "Active_Power";
+   public static final String PDU_APPARENT_POWER = "Apparent_Power";
+   public static final String PDU_CURRENT = "Current";
+   public static final String PDU_FREE_CAPACITY = "Free_Capacity";
+   public static final String PDU_TOTAL_CURRENT = "Total_Current";
+   public static final String PDU_TOTAL_POWER = "Total_Power";
+   public static final String PDU_CURRENT_LOAD = "Current_Load";
+   public static final String PDU_POWER_LOAD = "Power_Load";
+
+   public static final String SERVER_FRONT_TEMPERATURE = "Front_Temperature";
+   public static final String SERVER_BACK_TEMPREATURE = "Back_Temperature";
+   public static final String SERVER_HUMIDITY = "Humidity";
+   public static final String SERVER_TOTAL_CURRENT = "Total_Current";
+   public static final String SERVER_TOTAL_POWER = "Total_Power";
+   public static final String SERVER_VOLTAGE = "Voltage";
+   public static final String SERVER_CONNECTED_PDU_CURRENT = "Pdu_Current";
+   public static final String SERVER_CONNECTED_PDU_POWER = "Pdu_Power";
+   public static final String SERVER_USED_PDU_OUTLET_CURRENT = "Outlet_Current";
+   public static final String SERVER_USED_PDU_OUTLET_POWER = "Outlet_Power";
+}
