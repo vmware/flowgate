777f05da83b910d0f3588f4d06be9fc62ba48b32
Pengpeng Wang <46948228+Pengpengwanga@users.noreply.github.com>
2020-03-17 17:05:31 +0800

    Remove pduids for server (#306)
    Signed-off-by: Pengpeng Wang <pengpengw@vmware.com>

--- aggregator/src/test/java/com/vmware/flowgate/aggregator/MessageProcessingTest.java
+++ aggregator/src/test/java/com/vmware/flowgate/aggregator/MessageProcessingTest.java
@@ -5,8 +5,11 @@
 package com.vmware.flowgate.aggregator;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 
 import org.junit.Assert;
@@ -20,6 +23,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.vmware.flowgate.aggregator.scheduler.job.AggregatorService;
 import com.vmware.flowgate.common.FlowgateConstant;
+import com.vmware.flowgate.common.MetricName;
 import com.vmware.flowgate.common.model.Asset;
 import com.vmware.flowgate.common.model.SDDCSoftwareConfig;
 import com.vmware.flowgate.common.model.SDDCSoftwareConfig.SoftwareType;
@@ -119,5 +123,55 @@ public class MessageProcessingTest {
       }
 
       TestCase.assertEquals("INLET",aggregatorService.getPositionInfo(asset4));
+   }
+
+   @Test
+   public void testRemovePduFromServer() {
+      Asset[] servers = new Asset[2];
+      Asset server = new Asset();
+      List<String> pduids = new ArrayList<String>();
+      pduids.add("qwiounasdyi2avewrasdf");
+      pduids.add("mienoas2389asddsfqzda");
+      server.setPdus(pduids);
+      HashMap<String,String> justficationfields = new HashMap<String,String>();
+      String pduPorts = "01"+FlowgateConstant.SEPARATOR+"pdu1"+FlowgateConstant.SEPARATOR+"01"+FlowgateConstant.SEPARATOR+"qwepouasdnlksaydasmnd"+FlowgateConstant.SPILIT_FLAG
+            + "02"+FlowgateConstant.SEPARATOR+"pdu2"+FlowgateConstant.SEPARATOR+"02"+FlowgateConstant.SEPARATOR+"mienoas2389asddsfqzda";
+      justficationfields.put(FlowgateConstant.PDU_PORT_FOR_SERVER, pduPorts);
+      server.setJustificationfields(justficationfields);
+      Map<String,Map<String,Map<String,String>>> metricsFormular =
+            new HashMap<String,Map<String,Map<String,String>>>();
+      Map<String,Map<String,String>> pduMetrics = new HashMap<String,Map<String,String>>();
+      Map<String,String> pduinfo1 = new HashMap<String,String>();
+      pduinfo1.put(MetricName.PDU_ACTIVE_POWER, "mienoas2389asddsfqzda");
+      pduMetrics.put("mienoas2389asddsfqzda", pduinfo1);
+
+      Map<String,String> pduinfo2 = new HashMap<String,String>();
+      pduinfo2.put(MetricName.PDU_ACTIVE_POWER, "asdasdw2213dsdfaewwqe");
+      pduMetrics.put("asdasdw2213dsdfaewwqe", pduinfo2);
+      metricsFormular.put(FlowgateConstant.PDU, pduMetrics);
+      server.setMetricsformulars(metricsFormular);
+      servers[0] = server;
+
+      Asset server1 = new Asset();
+      List<String> pduids1 = new ArrayList<String>();
+      pduids1.add("asdasdasd");
+      pduids1.add("qweertwtc");
+      server1.setPdus(pduids1);
+      servers[1] = server1;
+
+      HashSet<String> pduAssetIDs = new HashSet<String>();
+      pduAssetIDs.add("mienoas2389asddsfqzda");
+      pduAssetIDs.add("asdw2cvxcjchftyhretyv");
+      List<Asset> needToUpdateServer = aggregatorService.removePduFromServer(servers, pduAssetIDs);
+      TestCase.assertEquals(1, needToUpdateServer.size());
+      Asset needupdateServer = needToUpdateServer.get(0);
+      TestCase.assertEquals("01"+FlowgateConstant.SEPARATOR+"pdu1"+FlowgateConstant.SEPARATOR+"01"+FlowgateConstant.SEPARATOR+"qwepouasdnlksaydasmnd",
+            needupdateServer.getJustificationfields().get(FlowgateConstant.PDU_PORT_FOR_SERVER));
+      TestCase.assertEquals(1,needupdateServer.getPdus().size());
+      TestCase.assertEquals("qwiounasdyi2avewrasdf", needupdateServer.getPdus().get(0));
+      Map<String,Map<String,Map<String,String>>> metricsFormulars =
+            needupdateServer.getMetricsformulars();
+      TestCase.assertEquals(1, metricsFormulars.size());
+      TestCase.assertEquals("asdasdw2213dsdfaewwqe", metricsFormulars.get(FlowgateConstant.PDU).keySet().iterator().next());
    }
 }
