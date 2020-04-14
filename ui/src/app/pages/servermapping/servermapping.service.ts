6b82bc8aed5c8341be439aa9cfc55fdfa6b51acf
Pengpeng Wang <pengpengw@vmware.com>
2020-04-14 15:34:28 +0800

    Manual map pdu and switch for server asset
    Signed-off-by: Pengpeng Wang <pengpengw@vmware.com>

--- ui/src/app/pages/servermapping/servermapping.service.ts
+++ ui/src/app/pages/servermapping/servermapping.service.ts
@@ -8,6 +8,8 @@ import { Headers, URLSearchParams } from '@angular/http';
 import 'rxjs/add/operator/map'
 import { AuthenticationService } from '../auth/authenticationService';
 import {environment} from 'environments/environment.prod';
+import { Observable } from 'rxjs';
+import { AssetModule } from './asset.module';
 
 @Injectable()
 export class ServermappingService {
@@ -41,25 +43,34 @@ export class ServermappingService {
    
   }
 
-  getAssets(pageSzie,pageNumber,keywords){
+  getAssets(pageSzie,pageNumber,keywords,category){
   let header = new Headers({ 'Content-Type': 'application/json' });
     header.append("Authorization",'Bearer ' + this.auth.getToken());
     this.options = new RequestOptions({ headers: header });
     let url = "";
     if(keywords == ""){
-      url = ""+this.API_URL+"/v1/assets/page/"+pageNumber+"/pagesize/"+pageSzie+"";
+      url = ""+this.API_URL+"/v1/assets/page/"+pageNumber+"/pagesize/"+pageSzie+"?category="+category;
     }else{
-      url = ""+this.API_URL+"/v1/assets/page/"+pageNumber+"/pagesize/"+pageSzie+"/keywords/"+keywords+"";
+      url = ""+this.API_URL+"/v1/assets/page/"+pageNumber+"/pagesize/"+pageSzie+"/keywords/"+keywords+"?category="+category;
     }
     return this.http.get(url,this.options)
     .map((res)=>res)
   }
 
-  getAssetById(id:string){
+  getAssetById(id:string):Observable<any>{
     let header = new Headers({ 'Content-Type': 'application/json' });
     header.append("Authorization",'Bearer ' + this.auth.getToken());
     this.options = new RequestOptions({ headers: header });
-    return this.http.get(""+this.API_URL+"/v1/assets/"+id,this.options).map((res)=>res)
+    return this.http.get(""+this.API_URL+"/v1/assets/"+id,this.options);
+  }
+
+  updateAsset(asset:AssetModule){
+    let header = new Headers({ 'Content-Type': 'application/json' });
+    header.append("Authorization",'Bearer ' + this.auth.getToken());
+    this.options = new RequestOptions({ headers: header });
+    let body = JSON.stringify(asset);
+  
+    return this.http.put(""+this.API_URL+"/v1/assets", body,this.options).map((res)=>res)
   }
   getMappingById(id:string){
     let header = new Headers({ 'Content-Type': 'application/json' });
