c4ba0508349604a121aa26bcc96aecdaccc73759
Pengpeng Wang <pengpengw@vmware.com>
2020-04-15 17:30:41 +0800

    update sql
    Signed-off-by: Pengpeng Wang <pengpengw@vmware.com>

--- flowgate-api/src/main/java/com/vmware/flowgate/repository/SystemSummaryRepository.java
+++ flowgate-api/src/main/java/com/vmware/flowgate/repository/SystemSummaryRepository.java
@@ -19,25 +19,25 @@ public interface SystemSummaryRepository
    public static final String hackid = "hid";
    public static final int cas = 89657;
 
-   @Query("SELECT COUNT(_class) AS count FROM #{#n1ql.bucket} WHERE _class = $1")
+   @Query("SELECT COUNT(*) AS count FROM #{#n1ql.bucket} WHERE _class = $1")
    int countByClass(String classpath);
 
-   @Query("SELECT COUNT(1) AS count FROM #{#n1ql.bucket} WHERE _class = $1 and type = $2")
+   @Query("SELECT COUNT(*) AS count FROM #{#n1ql.bucket} WHERE _class = $1 and type = $2")
    int countByClassAndType(String classpath,String value);
 
-   @Query("SELECT COUNT(1) AS count FROM #{#n1ql.bucket} WHERE _class = 'com.vmware.flowgate.common.model.ServerMapping' and vcID = $1")
+   @Query("SELECT COUNT(*) AS count FROM #{#n1ql.bucket} WHERE _class = 'com.vmware.flowgate.common.model.ServerMapping' and vcID = $1")
    int countServerMappingByVC(String vcID);
 
-   @Query("SELECT COUNT(1) AS count FROM #{#n1ql.bucket} WHERE _class = 'com.vmware.flowgate.common.model.ServerMapping' and vroID = $1")
+   @Query("SELECT COUNT(*) AS count FROM #{#n1ql.bucket} WHERE _class = 'com.vmware.flowgate.common.model.ServerMapping' and vroID = $1")
    int countServerMappingByVRO(String vroID);
 
-   @Query("SELECT count(category) AS count,category,\""+hackid+"\" as _ID, "+cas+" as _CAS FROM #{#n1ql.bucket} WHERE _class = 'com.vmware.flowgate.common.model.Asset' GROUP BY category")
+   @Query("SELECT count(*) AS count,category,\""+hackid+"\" as _ID, "+cas+" as _CAS FROM #{#n1ql.bucket} WHERE _class = 'com.vmware.flowgate.common.model.Asset' GROUP BY category")
    List<HashMap<String,Object>> countAssetGroupByType();
 
-   @Query("SELECT count(subCategory) AS count, subCategory, \""+hackid+"\" as _ID, "+cas+" as _CAS FROM #{#n1ql.bucket} WHERE _class = 'com.vmware.flowgate.common.model.Asset' AND category = 'Sensors' GROUP BY subCategory")
+   @Query("SELECT count(*) AS count, subCategory, \""+hackid+"\" as _ID, "+cas+" as _CAS FROM #{#n1ql.bucket} WHERE _class = 'com.vmware.flowgate.common.model.Asset' AND category = 'Sensors' GROUP BY subCategory")
    List<HashMap<String,Object>> countSensorGroupByType();
 
-   @Query("SELECT count(category) AS count, category, \""+hackid+"\" as _ID, "+cas+" as _CAS FROM #{#n1ql.bucket} WHERE _class = 'com.vmware.flowgate.common.model.Asset' AND assetSource like $1 GROUP BY category")
+   @Query("SELECT count(*) AS count, category, \""+hackid+"\" as _ID, "+cas+" as _CAS FROM #{#n1ql.bucket} WHERE _class = 'com.vmware.flowgate.common.model.Asset' AND assetSource like $1 GROUP BY category")
    List<HashMap<String,Object>> countAssetGroupByTypeAndSource(String assetSource);
 
 }
