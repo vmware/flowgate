375b3749c3e90f0af5127b06e590494b60d0fcf7
Pengpeng Wang <pengpengw@vmware.com>
2020-10-19 17:00:38 +0800

    Host metrics
    Signed-off-by: Pengpeng Wang <pengpengw@vmware.com>

--- flowgate-common/src/main/java/com/vmware/flowgate/common/MetricName.java
+++ flowgate-common/src/main/java/com/vmware/flowgate/common/MetricName.java
@@ -33,18 +33,18 @@ public class MetricName {
    public static final String SERVER_CONNECTED_PDU_POWER = "PduPower";
    public static final String SERVER_USED_PDU_OUTLET_CURRENT = "ConnectedPDUOutletCurrent";
    public static final String SERVER_USED_PDU_OUTLET_POWER = "ConnectedPDUOutletPower";
-   
-   public static final String VC_HOST_CPUUSEDINMHZ = "CpuUsedInMhz";
-   public static final String VC_HOST_CPUUSAGE = "CpuUsage";
-   public static final String VC_HOST_ACTIVEMEMORY = "ActiveMemory";
-   public static final String VC_HOST_SWAPMEMORY = "SwapMemory";
-   public static final String VC_HOST_SHAREDMEMORY = "SharedMemory";
-   public static final String VC_HOST_BALLOONMEMORY = "BalloonMemory";
-   public static final String VC_HOST_CONSUMEDMEMORY = "ConsumedMemory";
-   public static final String VC_HOST_MEMORYUSAGE = "MemoryUsage";
-   public static final String VC_HOST_STORAGEUSED = "StorageUsed";
-   public static final String VC_HOST_STORAGEUSAGE = "StorageUsage";
-   public static final String VC_HOST_STORAGEIORATEUSAGE = "StorageIORateUsage";
-   public static final String VC_HOST_NETWORKUTILIZATION = "NetworkUtilization";
+
+   public static final String VC_HOST_CPUUSEDINMHZ = "CpuUsedInMhz";//(MHZ)
+   public static final String VC_HOST_CPUUSAGE = "CpuUsage";//(Percentage)
+   public static final String VC_HOST_ACTIVEMEMORY = "ActiveMemory";//(KB)
+   public static final String VC_HOST_SWAPMEMORY = "SwapMemory";//(KB)
+   public static final String VC_HOST_SHAREDMEMORY = "SharedMemory";//(KB)
+   public static final String VC_HOST_BALLOONMEMORY = "BalloonMemory";//(KB)
+   public static final String VC_HOST_CONSUMEDMEMORY = "ConsumedMemory";//(KB)
+   public static final String VC_HOST_MEMORYUSAGE = "MemoryUsage";//(Percentage)
+   public static final String VC_HOST_STORAGEUSED = "StorageUsed";//(MB)
+   public static final String VC_HOST_STORAGEUSAGE = "StorageUsage";//(Percentage)
+   public static final String VC_HOST_STORAGEIORATEUSAGE = "StorageIORateUsage";//(KBps)
+   public static final String VC_HOST_NETWORKUTILIZATION = "NetworkUtilization";//(KBps)Combined transmit-rates and receive-rates
 
 }
