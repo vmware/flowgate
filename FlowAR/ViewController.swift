7821c5d1bd053671ac077a2d0930afab6db47e53
Shuyi Zhou <zhoushuyi495@gmail.com>
2020-10-20 22:37:48 +0800

    Initial Commit

---
+++ FlowAR/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FlowAR
+//
+//  Created by 周舒意 on 2020/10/20.
+//  Copyright © 2020 周舒意. All rights reserved.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
