f98e4e656807f5bb849d79d5a3bf6af0cbbe7ac8
tcy1999 <tcy9902@outlook.com>
2020-11-25 00:17:14 +0800

    add reset
    Signed-off-by: tcy1999 <tcy9902@outlook.com>

--- augmentedimage/app/src/main/java/com/google/ar/sceneform/samples/augmentedimage/AugmentedImageActivity.java
+++ augmentedimage/app/src/main/java/com/google/ar/sceneform/samples/augmentedimage/AugmentedImageActivity.java
@@ -19,12 +19,14 @@ package com.google.ar.sceneform.samples.augmentedimage;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.ImageFormat;
+import android.graphics.Rect;
 import android.media.Image;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -49,8 +51,6 @@ import java.util.HashMap;
 import java.util.Map;
 
 
-
-
 /**
  * This application demonstrates using augmented images to place anchor nodes. app to include image
  * tracking functionality.
@@ -63,13 +63,16 @@ import java.util.Map;
  * Images</a>.
  */
 public class AugmentedImageActivity extends AppCompatActivity {
-
   private ArFragment arFragment;
   private ImageView fitToScanView;
+  private Button button;
 
+  private TextView serverTextview, cabinetTextview;
+  private ViewRenderable serverRenderable, cabinetRenderable;
 
-  private ViewRenderable testRenderable;
-  private AnchorNode anchorNode;
+  private AnchorNode serverNode, cabinetNode;
+
+  private Boolean isScanSuccess = false;
 
   flowgateClient fc = new flowgateClient("202.121.180.32", "admin", "Ar_InDataCenter_450");
 
@@ -84,14 +87,33 @@ public class AugmentedImageActivity extends AppCompatActivity {
 
     arFragment = (ArFragment) getSupportFragmentManager().findFragmentById(R.id.ux_fragment);
     fitToScanView = findViewById(R.id.image_view_fit_to_scan);
+    button = findViewById(R.id.button1);
 
-    // Build the 2D renderable
+    // Reset the app status.
+    button.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        isScanSuccess = false;
+        if (serverTextview != null){
+          serverTextview.setText("");
+        }
+        if (cabinetTextview != null){
+          cabinetTextview.setText("");
+        }
+      }
+    });
+
+    // Build the 2D renderable for text views of server & cabinet.
     ViewRenderable.builder()
-            .setView(this, R.layout.card_view)
+            .setView(this, R.layout.server_view)
             .build()
-            .thenAccept(renderable -> testRenderable = renderable);
+            .thenAccept(renderable -> serverRenderable = renderable);
+    ViewRenderable.builder()
+        .setView(this, R.layout.cabinet_view)
+        .build()
+        .thenAccept(renderable -> cabinetRenderable = renderable);
 
-    arFragment.getArSceneView().getScene().addOnUpdateListener(this::onSceneUpdate);
+    arFragment.getArSceneView().getScene().addOnUpdateListener(this::onUpdateFrame);
   }
 
   @Override
@@ -108,96 +130,109 @@ public class AugmentedImageActivity extends AppCompatActivity {
    *
    * @param frameTime - time since last frame.
    */
-  //private void onUpdateFrame(FrameTime frameTime) {
-  private void onSceneUpdate(FrameTime frameTime) {
+  private void onUpdateFrame(FrameTime frameTime) {
     Frame frame = arFragment.getArSceneView().getArFrame();
-    // Set Auto focus of the camera
     Session session = arFragment.getArSceneView().getSession();
 
-
-    Config config = null;
-    if (session != null) {
-      config = session.getConfig();
-      config.setFocusMode(Config.FocusMode.AUTO);
-      session.configure(config);
-    }
-
-    // If there is no frame, just return.
-    if (frame == null) {
+    if (session == null || frame == null) {
       return;
     }
+
+    // Set Auto focus of the camera.
+    Config config = session.getConfig();
+    config.setFocusMode(Config.FocusMode.AUTO);
+    session.configure(config);
 
     // Let the fragment update its state first.
     arFragment.onUpdate(frameTime);
 
-    // If there is no frame then don't process anything.
-    if (arFragment.getArSceneView().getArFrame() == null) {
-      return;
-    }
-
     // If ARCore is not tracking yet, then don't process anything.
-    if (arFragment.getArSceneView().getArFrame().getCamera().getTrackingState() != TrackingState.TRACKING) {
+    if (frame.getCamera().getTrackingState() != TrackingState.TRACKING) {
       return;
     }
 
-    // Place the anchor 1m in front of the camera if anchorNode is null.
-    if (this.anchorNode == null && session != null) {
-      // Session session = arFragment.getArSceneView().getSession();
-      float[] pos = {0,0,-1};
-      float[] rotation = {0,0,0,1};
-      Anchor anchor = session.createAnchor(new Pose(pos, rotation));
-      anchorNode = new AnchorNode(anchor);
-      anchorNode.setRenderable(testRenderable);
-      anchorNode.setParent(arFragment.getArSceneView().getScene());
-    }
-
-    // Send frame image and scan barcode
-    try (final Image image = arFragment.getArSceneView().getArFrame().acquireCameraImage()) {
-      if (image.getFormat() == ImageFormat.YUV_420_888) {
-        Bitmap bitmapImage = YUV420toBitmap.getBitmap(image);
-        BarcodeScan barScanning = new BarcodeScan();
-        InputImage inputImage = InputImage.fromBitmap(bitmapImage, 0);
-
-        TextView textView = (TextView) testRenderable.getView();
-        Context context = this.getApplicationContext();
-
-        barScanning.scanBarcodes(inputImage, fc, context, textView);
-        image.close();
+    serverTextview = (TextView) serverRenderable.getView();
+    cabinetTextview = (TextView) cabinetRenderable.getView();
+    // Rect barcodePos = null;
+    // Scan once; and once the information is obtained, no scanning before reset.
+    if (!isScanSuccess) {
+      if (this.serverNode != null) {
+        serverNode.getAnchor().detach();
+        serverNode.setParent(null);
+        serverNode.setRenderable(null);
+        serverNode = null;
       }
-    } catch (NotYetAvailableException e) {
-      Log.e("TAG", e.getMessage());
-    }
+      // Send frame image and scan barcode.
+      try (final Image image = arFragment.getArSceneView().getArFrame().acquireCameraImage()) {
+        if (image.getFormat() == ImageFormat.YUV_420_888) {
+          Bitmap bitmapImage = YUV420toBitmap.getBitmap(image);
+          BarcodeScan barScanning = new BarcodeScan();
+          InputImage inputImage = InputImage.fromBitmap(bitmapImage, 0);
 
-    Collection<AugmentedImage> updatedAugmentedImages =
-            frame.getUpdatedTrackables(AugmentedImage.class);
-    for (AugmentedImage augmentedImage : updatedAugmentedImages) {
-      switch (augmentedImage.getTrackingState()) {
-        case PAUSED:
-          // When an image is in PAUSED state, but the camera is not PAUSED, it has been detected,
-          // but not yet tracked.
-          String text = "Detected Image " + augmentedImage.getIndex();
-          SnackbarHelper.getInstance().showMessage(this, text);
-          break;
+          Context context = this.getApplicationContext();
 
-        case TRACKING:
-          // Have to switch to UI Thread to update View.
-          fitToScanView.setVisibility(View.GONE);
-
-          // Create a new anchor for newly found images.
-          if (!augmentedImageMap.containsKey(augmentedImage)) {
-            AugmentedImageNode node = new AugmentedImageNode();
-            node.setImage(augmentedImage, this);
-            augmentedImageMap.put(augmentedImage, node);
-            arFragment.getArSceneView().getScene().addChild(node);
-          }
-          break;
-
-        case STOPPED:
-          augmentedImageMap.remove(augmentedImage);
-          break;
+          barScanning.scanBarcodes(inputImage, fc, context, serverTextview);
+          image.close();
+        }
+      } catch (NotYetAvailableException e) {
+        Log.e("Barcode", e.getMessage());
       }
     }
 
+    isScanSuccess = (serverTextview.getText().toString().length() != 0);
+    // Image recognition after barcode scanning.
+    if (isScanSuccess) {
+      // Put the server's information near the barcode.
+      if (this.serverNode == null) {
+        // float[] pos = {barcodePos.right, barcodePos.top, -1};
+        float[] pos = {0, 0, -1};
+        float[] rotation = {0, 0, 0, 1};
+        Anchor anchor = session.createAnchor(new Pose(pos, rotation));
+        serverNode = new AnchorNode(anchor);
+        serverNode.setRenderable(serverRenderable);
+        serverNode.setParent(arFragment.getArSceneView().getScene());
+      }
 
+      Collection<AugmentedImage> updatedAugmentedImages =
+          frame.getUpdatedTrackables(AugmentedImage.class);
+      for (AugmentedImage augmentedImage : updatedAugmentedImages) {
+        switch (augmentedImage.getTrackingState()) {
+          case PAUSED:
+            // When an image is in PAUSED state, but the camera is not PAUSED, it has been detected,
+            // but not yet tracked.
+            String text = "Detected Image " + augmentedImage.getIndex();
+            SnackbarHelper.getInstance().showMessage(this, text);
+            break;
+
+          case TRACKING:
+            // Have to switch to UI Thread to update View.
+            fitToScanView.setVisibility(View.GONE);
+
+            // Create a new anchor for newly found images.
+            if (!augmentedImageMap.containsKey(augmentedImage)) {
+              AugmentedImageNode node = new AugmentedImageNode();
+              node.setImage(augmentedImage, this);
+              augmentedImageMap.put(augmentedImage, node);
+              arFragment.getArSceneView().getScene().addChild(node);
+
+              /*// Create the anchor near the cabinet to show the text.
+              // 传fc, context, cabinetTextview
+              if (this.cabinetNode == null) {
+                float[] pos = {0, 0.0f, 0.5f * augmentedImage.getExtentZ()};
+                float[] rotation = {0, 0, 0, 1};
+                Anchor anchor = session.createAnchor(new Pose(pos, rotation));
+                cabinetNode = new AnchorNode(anchor);
+                cabinetNode.setRenderable(cabinetRenderable);
+                cabinetNode.setParent(arFragment.getArSceneView().getScene());
+              }*/
+            }
+            break;
+
+          case STOPPED:
+            augmentedImageMap.remove(augmentedImage);
+            break;
+        }
+      }
+    }
   }
 }
