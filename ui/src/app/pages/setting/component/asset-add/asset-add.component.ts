a60905a9222030371c77ff0e2e6f9e64c2d29233
Pengpeng Wang <pengpengw@vmware.com>
2020-11-25 11:46:13 +0800

    Modify pages of asset
    Signed-off-by: Pengpeng Wang <pengpengw@vmware.com>

--- ui/src/app/pages/setting/component/asset-add/asset-add.component.ts
+++ ui/src/app/pages/setting/component/asset-add/asset-add.component.ts
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
 import { AssetModule } from '../asset-modules/asset.module';
 import {Router,ActivatedRoute} from '@angular/router';
 import { SettingService } from '../../setting.service';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { AssetRealtimeDataSpecModule } from '../asset-modules/assetrealtimedataspec.module';
+import { AssetStatusModule } from '../asset-modules/assetstatus.module';
 @Component({
   selector: 'app-asset-add',
   templateUrl: './asset-add.component.html',
@@ -20,18 +23,95 @@ export class AssetAddComponent implements OnInit {
   allcategory=[ "Server", "PDU","Cabinet","Networks","Sensors","UPS","Chassis"];
   suballcategory = {"Server":["Blade","Standard"], 
   "Sensors":["Humidity","Temperature","AirPressure","AirFlow","ContactClosure","Smoke","Water","Vibration"]};
-
-  constructor(private service:SettingService,private router:Router) { }
+  addAssetForm:FormGroup;
+  constructor(private service:SettingService,private router:Router,private fb: FormBuilder) { 
+    this.addAssetForm = this.fb.group({
+      category: ['', [
+        Validators.required
+      ]],
+      assetName: ['', [
+        Validators.required
+      ]],
+      subCategory: ['', [
+        Validators.required
+      ]],
+      mountingSide: ['', [
+        Validators.required
+      ]],
+      assetNumber: ['', [
+      ]],
+      manufacturer: ['', [
+      ]],
+      model: ['', [
+      ]],
+      serialnumber: ['', [
+      ]],
+      tag: ['', [
+      ]],
+      cabinetName: ['', [
+      ]],
+      cabinetUnitPosition: ['', [
+      ]],
+      capacity: ['', [
+      ]],
+      cabinetAssetNumber: ['', [
+      ]],
+      region: ['', [
+      ]],
+      country: ['', [
+      ]],
+      city: ['', [
+      ]],
+      building: ['', [
+      ]],
+      floor: ['', [
+      ]],
+      room: ['', [
+      ]],
+      row: ['', [
+      ]],
+      col: ['', [
+      ]],
+      unit: ['', [
+      ]],
+      validNumMin: ['', [
+      ]],
+      validNumMax: ['', [
+      ]],
+      status: ['', [
+        Validators.required
+      ]]
+    });
+  }
+  changeType(){
+    let category:string = this.addAssetForm.get('category').value;
+    if(category == 'Sensors'){
+      this.addAssetForm.setControl("unit",new FormControl('',Validators.required));
+    }
+  }
   ngOnInit() {
   }
   closefailTips(){
     this.addfail=false;
   }
-  cancel(){ this.router.navigate(["/ui/nav/setting/asset-list"]);}
+  cancel(){ 
+    this.router.navigate(["/ui/nav/setting/asset-list"]);
+  }
   save(){
+    let asset:AssetModule = this.addAssetForm.value;
+    if(asset.category == 'Sensors'){
+      let realtimespec:AssetRealtimeDataSpecModule = new AssetRealtimeDataSpecModule();
+      realtimespec.unit = this.addAssetForm.get('unit').value;
+      realtimespec.validNumMax = this.addAssetForm.get('validNumMax').value;
+      realtimespec.validNumMin = this.addAssetForm.get('validNumMin').value;
+      asset.assetRealtimeDataSpec = realtimespec;
+    }
     this.addassetloading = true;
-    this.asset.assetSource = "flowgate";
-    this.service.createAnAsset(this.asset).subscribe(
+    asset.assetSource = "flowgate";
+    let assetStatus:AssetStatusModule = new AssetStatusModule();
+    assetStatus.status = this.addAssetForm.get('status').value;
+    asset.status = assetStatus
+    this.service.createAnAsset(asset).subscribe(
       (data)=>{
         this.addassetloading = false;
         this.router.navigate(["/ui/nav/setting/asset-list"]);
