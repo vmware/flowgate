e18e4e743116742283a3391e5757432ad8552f17
Pengpeng Wang <pengpengw@vmware.com>
2020-11-30 16:33:24 +0800

    Modify pages of role management
    Signed-off-by: Pengpeng Wang <pengpengw@vmware.com>

--- ui/src/app/pages/facility/component/dcim/dcim-list/dcim-list.component.ts
+++ ui/src/app/pages/facility/component/dcim/dcim-list/dcim-list.component.ts
@@ -127,14 +127,6 @@ export class DcimListComponent implements OnInit {
       this.refresh(this.currentState)
     }
   }
-  createTime(time){
-		var da = time;
-	    da = new Date(da);
-	    var year = da.getFullYear()+'-';
-	    var month = da.getMonth()+1+'-';
-	    var date = da.getDate();
-	    return year+month+date;
-  }
   loading:boolean = true;
   currentState:ClrDatagridStateInterface;
   totalItems:number = 0;
