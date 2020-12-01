234d03afeb93df33c2579e8ec7585fbf5a0dd69a
haoyul <haoyul@vmware.com>
2020-12-01 12:00:18 +0800

    update edit asset page
    Signed-off-by: haoyul <haoyul@vmware.com>

--- ui/src/app/pages/setting/component/asset-edit/asset-edit.component.ts
+++ ui/src/app/pages/setting/component/asset-edit/asset-edit.component.ts
@@ -4,8 +4,13 @@
 */
 import { Component, OnInit } from '@angular/core';
 import { AssetModule } from '../asset-modules/asset.module';
-import { Router,ActivatedRoute } from '@angular/router';
+import {Router,ActivatedRoute} from '@angular/router';
 import { SettingService } from '../../setting.service';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { AssetRealtimeDataSpecModule } from '../asset-modules/assetrealtimedataspec.module';
+import { AssetStatusModule } from '../asset-modules/assetstatus.module';
+import { AssetTempModule } from '../asset-modules/asset-temp.module';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-asset-edit',
@@ -15,7 +20,7 @@ import { SettingService } from '../../setting.service';
 export class AssetEditComponent implements OnInit {
   description:any;
   asset:AssetModule = new AssetModule();
-  addfail:boolean =false;
+  editfail:boolean =false;
   editassetloading:boolean = false;
   tip:string = " ";
   allcategory=[ "Server", "PDU","Cabinet","Networks","Sensors","UPS","Chassis"];
@@ -27,35 +32,156 @@ export class AssetEditComponent implements OnInit {
   jsonerror:boolean = false;
   checkjsonformat:boolean = false;
   jsonerrormsg:string = " ";
-
-  constructor(private service:SettingService,private router:Router,private activedRoute:ActivatedRoute) { }
+  editAssetForm:FormGroup;
+  constructor(private service:SettingService,private router:Router,private fb: FormBuilder, private activedRoute:ActivatedRoute) { 
+    this.editAssetForm = this.fb.group({
+      category: ['', [
+        Validators.required
+      ]],
+      assetName: ['', [
+        Validators.required
+      ]],
+      subCategory: ['', [
+        Validators.required
+      ]],
+      mountingSide: ['', [
+        Validators.required
+      ]],
+      assetNumber: ['', [
+      ]],
+      manufacturer: ['', [
+      ]],
+      model: ['', [
+      ]],
+      serialnumber: ['', [
+      ]],
+      tag: ['', [
+      ]],
+      cabinetName: ['', [
+      ]],
+      cabinetUnitPosition: ['', [
+      ]],
+      capacity: ['', [
+      ]],
+      cabinetAssetNumber: ['', [
+      ]],
+      region: ['', [
+      ]],
+      country: ['', [
+      ]],
+      city: ['', [
+      ]],
+      building: ['', [
+      ]],
+      floor: ['', [
+      ]],
+      room: ['', [
+      ]],
+      row: ['', [
+      ]],
+      col: ['', [
+      ]],
+      unit: ['', [
+      ]],
+      validNumMin: ['', [
+      ]],
+      validNumMax: ['', [
+      ]],
+      status:  ['', [
+      ]],
+      statusDisplay: ['', [
+        Validators.required
+      ]],
+      assetRealtimeDataSpec: ['', [
+      ]],
+      id: ['', [
+      ]],
+      assetSource: ['', [
+      ]],
+      assetAddress: ['', [
+      ]],
+      extraLocation: ['', [
+      ]],
+      freeCapacity: ['', [
+      ]],
+      justificationfields: ['', [
+      ]],
+      parent: ['', [
+      ]],
+      pdus: ['', [
+      ]],
+      switches: ['', [
+      ]],
+      metricsformulars: ['', [
+      ]],
+      lastupdate: ['', [
+      ]],
+      created: ['', [
+      ]],
+      tenant: ['', [
+      ]]
+    });
+  }
+  changeType(){
+    let category:string = this.editAssetForm.get('category').value;
+    if(category == 'Sensors'){
+      this.editAssetForm.setControl("unit",new FormControl('',Validators.required));
+    }
+  }
   ngOnInit() {
     this.asset.id = this.activedRoute.snapshot.params['id'];
     if(this.asset.id != null && this.asset.id != ""){
       this.service.getAssetsByID(this.asset.id).subscribe(
-        (data:AssetModule)=>{
-          this.asset = data;
+        (assetTemp:AssetTempModule)=>{
+          this.asset = assetTemp;
+          assetTemp.unit = "";
+          assetTemp.validNumMax = "";
+          assetTemp.validNumMin = "";
+          assetTemp.statusDisplay = "";
+          if(assetTemp.assetRealtimeDataSpec != null){
+            assetTemp.unit = assetTemp.assetRealtimeDataSpec.unit == null ? "": assetTemp.assetRealtimeDataSpec.unit;
+            assetTemp.validNumMax = assetTemp.assetRealtimeDataSpec.validNumMax == null ? "": assetTemp.assetRealtimeDataSpec.validNumMax;
+            assetTemp.validNumMin = assetTemp.assetRealtimeDataSpec.validNumMin == null ? "": assetTemp.assetRealtimeDataSpec.validNumMin;
+          }
+          if(assetTemp.status != null){
+            assetTemp.statusDisplay = assetTemp.status.status == null ? "": assetTemp.status.status;
+          }
+          this.assetjsondata = this.jsonFormater(JSON.stringify(this.asset, this.replacer));
+          this.editAssetForm.setValue(assetTemp);
         }
       )
     }
   }
   closefailTips(){
-    this.addfail=false;
+    this.editfail=false;
   }
-  cancel(){ this.router.navigate(["/ui/nav/setting/asset-list"]);}
+  cancel(){ 
+    this.router.navigate(["/ui/nav/setting/asset-list"]);
+  }
   jsonFormater(json:any) {
     var obj = JSON.parse(json);
     var formated = JSON.stringify(obj, undefined, 4);
     return formated;
   }
+
+  replacer(key, value)
+  {
+      if (key=="unit") return undefined;
+      else if (key=="validNumMin") return undefined;
+      else if (key=="validNumMax") return undefined;
+      else if (key=="statusDisplay") return undefined;
+      else return value;
+  }
+
   advanceCheckbox(){
     this.advance = !this.advance;
     this.baseconfig = !this.baseconfig;
-    this.assetjsondata = this.jsonFormater(JSON.stringify(this.asset));
   }
+
   checkJsonFormat(){
+    let jsondata = this.assetjsondata;
     try {
-      JSON.parse(this.assetjsondata);
+      JSON.parse(jsondata);
       this.checkjsonformat = false;
       this.jsonerror = false;
     } catch(e) {
@@ -66,32 +192,41 @@ export class AssetEditComponent implements OnInit {
   }
   save(){
 
+    let asset:AssetModule = new AssetModule();
     if(this.advance == true){
-      let assetobj = JSON.parse(this.assetjsondata);
-      if(assetobj.id != this.asset.id){
-        this.addfail = true;
+      let jsondata = this.assetjsondata;
+      asset = JSON.parse(jsondata);
+
+      if(asset.id != this.asset.id){
+        this.editfail = true;
         this.tip = "Don't change asset ID.";
         return;
       }
-      this.asset = JSON.parse(this.assetjsondata);
+    }else{
+      asset= this.editAssetForm.value;
+      if(asset.category == 'Sensors'){
+        let realtimespec:AssetRealtimeDataSpecModule = new AssetRealtimeDataSpecModule();
+        realtimespec.unit = this.editAssetForm.get('unit').value;
+        realtimespec.validNumMax = this.editAssetForm.get('validNumMax').value;
+        realtimespec.validNumMin = this.editAssetForm.get('validNumMin').value;
+        asset.assetRealtimeDataSpec = realtimespec;
+      }
+      this.editassetloading = true;
+      asset.assetSource = "flowgate";
+      let assetStatus:AssetStatusModule = new AssetStatusModule();
+      assetStatus.status = this.editAssetForm.get('statusDisplay').value;
+      asset.status = assetStatus
     }
-    
-    this.editassetloading = true;
-    this.service.updateAssetsByID(this.asset).subscribe(
+
+    this.service.updateAssetsByID(asset).subscribe(
       (data)=>{
         this.editassetloading = false;
         this.router.navigate(["/ui/nav/setting/asset-list"]);
-      },
-      error=>{
+      },(error:HttpErrorResponse)=>{
         this.editassetloading = false;
-        if(error.status == 400)
-        {
-          this.addfail =true;
-          this.tip = "Bad Request.";
-        }
         if(error.status == 500)
         {
-          this.addfail =true;
+          this.editfail =true;
           this.tip = "Internal Server Error.";
         }
       }
---
+++ ui/src/app/pages/setting/component/asset-modules/asset-temp.module.ts
@@ -0,0 +1,59 @@
+/**
+ * Copyright 2020 VMware, Inc.
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AssetStatusModule } from './assetstatus.module';
+import { AssetRealtimeDataSpecModule } from './assetrealtimedataspec.module';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ]
+})
+export class AssetTempModule {
+  id:string;
+  assetNumber:number;
+  assetName:string;
+  assetSource:string;
+  category:string;
+  subCategory:string;
+  manufacturer:string;
+  serialnumber:string;
+  model:string;
+  tag:string;
+  assetAddress:{};
+  region:string;
+  country:string;
+  city:string;
+  building:string;
+  floor:string;
+  room:string;
+  row:string;
+  col:string;
+  extraLocation:string;
+  cabinetName:string;
+  cabinetUnitPosition:number;
+  mountingSide:{};
+  capacity:number;
+  freeCapacity:number;
+  cabinetAssetNumber:string;
+  assetRealtimeDataSpec:AssetRealtimeDataSpecModule = new AssetRealtimeDataSpecModule();
+  justificationfields:any;
+  parent:any;
+  status:AssetStatusModule = new AssetStatusModule();
+  pdus:string[];
+  switches:string[];
+  metricsformulars:any;
+  lastupdate:number;
+  created:number;
+  tenant:any;
+  unit:string;
+  validNumMin:any;
+  validNumMax:any;
+  statusDisplay:any;
+ }
