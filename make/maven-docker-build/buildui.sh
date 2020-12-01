0b21caf3f280e2aa2e8297eb1f613abc5d9c171f
Pengpeng Wang <pengpengw@vmware.com>
2020-12-01 10:00:57 +0800

    Modify the version of angular-cli in buildui.sh
    Signed-off-by: Pengpeng Wang <pengpengw@vmware.com>

--- make/maven-docker-build/buildui.sh
+++ make/maven-docker-build/buildui.sh
@@ -10,5 +10,5 @@ fi
 cd $BUILDDIR/ui/
 npm install --unsafe-perm
 npm config set unsafe-perm true
-npm install -g @angular/cli@1.6.1
+npm install -g @angular/cli@10.2.0
 ng build --prod -e prod -aot=false >> $BUILDLOG
