a51a617e8ed98dc3f5900e0765f8608e803a3d68
Pengpeng Wang <pengpengw@vmware.com>
2021-03-22 11:52:16 +0800

    Fix tomcat CVE-2021-25329
    Signed-off-by: Pengpeng Wang <pengpengw@vmware.com>

--- poweriq-worker/src/main/java/com/vmware/flowgate/poweriqworker/client/PowerIQAPIClient.java
+++ poweriq-worker/src/main/java/com/vmware/flowgate/poweriqworker/client/PowerIQAPIClient.java
@@ -8,7 +8,7 @@ import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.tomcat.util.codec.binary.Base64;
+import org.apache.commons.codec.binary.Base64;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpEntity;
