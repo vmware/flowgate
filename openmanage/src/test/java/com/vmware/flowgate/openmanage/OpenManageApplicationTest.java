ad7c658de61d7510bbbcf0ae7ae49e56695f98ca
Yongchun Chang <yongchunc@vmware.com>
2023-06-09 15:47:44 +0800

    Fix security vulneralbilities.
    Signed-off-by: Yongchun Chang<yongchunc@vmware.com>

--- openmanage/src/test/java/com/vmware/flowgate/openmanage/OpenManageApplicationTest.java
+++ openmanage/src/test/java/com/vmware/flowgate/openmanage/OpenManageApplicationTest.java
@@ -4,13 +4,13 @@
 */
 package com.vmware.flowgate.openmanage;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(classes = {OpenManageApplicationTest.class})
 public class OpenManageApplicationTest {
 
 	@Test
